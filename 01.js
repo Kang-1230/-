2c5a66f9a93f7fef38e622c9f44336a8831fdb92
Kang-1230 <tnwls7138@gmail.com>
2024-07-24 12:28:50 +0900

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,80 @@
+1. let uninitialized;
+console.log(uninitialized); // 결과값 < undefined >
+ //암시적으로 정의되지 않은 값은 undefined가 뜨기 때문에
+
+2. < var 또는 let > apple = "사과";
+apple = "바나나"; // TypeError: Assignment to constant variable
+ //var는 재선언, 재할당 모두 가능하고, let은 재할당이 가능하기 때문
+
+3. let lotto = [3, 8, 13, 19, 21, 32];
+console.log(lotto[3]); // 결과값 < 19 >
+//배열의 인덱스는 0부터 시작하기 때문에
+
+4. 
+let mySchedule = "";
+console.log(mySchedule || false); // < false >
+console.log(!!mySchedule); // < "" >
+// ||은 앞 값이 false이면 뒷 값이 나오기 때문에
+// !!가 조금 헷갈리네요..아니의 아니니까 mySchedule의 값으로 생각했습니다
+
+//----------------------------------------------
+
+const junhyun = {
+    // 조건을 충족하는 코드 작성
+    name : "sujin",
+    age : 29,
+    MBTI : "INFP",
+};
+
+console.log(junhyun.name);
+console.log(junhyun.age);
+console.log(junhyun.MBTI);
+
+//----------------------------------------------
+
+function OddEven(x) {
+    if (x%2 === 0) {
+        return "짝수";
+    } else {
+        return "홀수";
+    }
+}
+
+console.log(OddEven(10)); // 결과값 "짝수";
+console.log(OddEven(7)); // 결과값 "홀수";
+
+//----------------------------------------------
+
+function Calculator(a, b, c) {
+    if (b === "+"){
+        return a+c;
+    } if (b === "-"){
+        return a-c;
+    } if (b === "*"){
+        return a*c;
+    } else { 
+        return a/c; }
+}
+
+function Calculator (a, b, c){
+    return a + Calculator[1] + c;
+}
+
+Calculator(3, "+", 6); // 결과값 9
+Calculator(11, "-", 6); // 결과값 5
+Calculator(6, "*", 3); // 결과값 18
+Calculator(15, "/", 3); // 결과값 5
+
+//b의 문자열에서 연산자만 가져오는 방법이 애매했음. 문자열의 경우 더하기 연산자면 문자열로 더해지기 때문에
+//코드 찍어보니까 값이 안나오는데 왜 그런지 모르겠음...
+//결국 하나하나 풀어서 쓰긴 했는데, 계산기면 다른 기호도 들어가야 할 텐데... 두 번째 solution이 왜 안될까?
+
+//-----------------------------------------------
+
+
+const scores = [36, 62, 72, 55, 86, 95, 92, 48, 81];
+
+console.log(scores);
+// 기대값: [39, 65, 75, 58, 89, 98, 95, 51, 84]
+
+//계산기를 어떻게 쓸지 모르겠음
